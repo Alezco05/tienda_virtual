fcbb8627166f5aeb8e11fe059a738a7a3b0c49aa
Alezco05 <jhon.mario.05hotmail.com>
2018-12-06 12:12:11 -0500

    Agregado las librerias y el navbar

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+</head>
+<body>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Mi sitio
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">1</a>
+          <a class="dropdown-item" href="#">2</a>
+          <a class="dropdown-item" href="#">3</a>
+        </div>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Cursos <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Libros <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Sobre mí <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Contacto <span class="sr-only">(current)</span></a>
+      </li>
+      <li>
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+  </div>
+</nav> -->
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+    <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Mi sitio</a>
+  <div class="collapse navbar-collapse" id="menu">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active"><a class="nav-link" href="#">Inicio</a></li>  
+      <li class="nav-item active"><a class="nav-link" href="#">Cursos</a></li>
+      <li class="nav-item active"><a class="nav-link" href="#">Libros</a></li>
+      <li class="nav-item active"><a class="nav-link" href="#">Sobre mí</a></li>
+      <li class="nav-item active"><a class="nav-link" href="#">Contacto</a></li>  
+    </ul>
+    <ul class="navbar-nav  navbar-right">
+    <li><a href="login.html"><i class="fas fa-sign-in-alt">Login</i></a></li>  
+    </ul>
+  </div>
+</nav>
+        
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body> 
+</html>
